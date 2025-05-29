7eca4ba1d38748a43b5d32f7d8fe13ae9892677a
Joseph Le Grice <joe.legrice@gmail.com>
2025-05-29 14:41:24 +0100

    no idea if getting closer or not

--- Runtime/Entities/BlobAssetScriptableObject.cs
+++ Runtime/Entities/BlobAssetScriptableObject.cs
@@ -1,25 +1,48 @@
+using System.Runtime.CompilerServices;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Entities.Content;
 using Unity.Entities.Serialization;
 using UnityEngine;
 using Hash128 = Unity.Entities.Hash128;
 
-public struct AssetLookup : IComponentData  // Workaround for WeakObjectReference<Object> not available in blobs yet
+public struct AssetLookup : IComponentData  // Workaround for WeakObjectReference<Object> not available in blobs yet - dodgy 
 {
+    public bool StartedLoad;
     public Hash128 AssetHash;
     public UntypedWeakReferenceId AssetReference;
 
-    public static UntypedWeakReferenceId FindAssetReference()
-    {
-        throw new System.NotImplementedException();
-    }
+    // [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    // public static void FindAssetReference<T>(Hash128 assetHash, ref T asset) where T : Object
+    // {
+    //     EntityManager em = World.DefaultGameObjectInjectionWorld.EntityManager;
+    //     EntityQuery query = em.CreateEntityQuery(ComponentType.ReadWrite<AssetLookup>());
+    //     foreach (AssetLookup prefabReference in query.ToComponentDataArray<AssetLookup>())
+    //     {
+    //         if (prefabReference.AssetHash == assetHash)
+    //         {
+    //             if (!prefabReference.StartedLoad)
+    //             {
+    //                 RuntimeContentManager.LoadObjectAsync(prefabReference.AssetReference);
+    //                 prefabReference.StartedLoad = true;
+    //             }
+    //             else if (RuntimeContentManager.GetObjectLoadingStatus(prefabReference.AssetReference) ==
+    //                      ObjectLoadingStatus.Completed)
+    //             {
+    //                 asset = RuntimeContentManager.GetObjectValue<T>(prefabReference.AssetReference);
+    //             }
+    //             
+    //             break;
+    //         }
+    //     }
+    // }
 }
 
 public abstract class BlobAssetScriptableObject<T> : ScriptableObject where T : unmanaged
 {
     protected abstract void PopulateBlob(IBaker baker, BlobBuilder builder, ref T blobData);
 
-    protected static Hash128 RegisterAssetReference(IBaker baker, Object asset) // Workaround for WeakObjectReference<Object> not available in blobs yet
+    protected static Hash128 RegisterAssetReference(IBaker baker, Object asset) // Workaround for WeakObjectReference<Object> not available in blobs yet - dodgy 
     {
         Hash128 assetHash =  new Hash128((uint)asset.GetHashCode());
         Entity assetRef = baker.CreateAdditionalEntity(TransformUsageFlags.None, entityName: "AssetLookup_"+asset.name); // May end up with duplicates?
@@ -32,7 +55,7 @@ public abstract class BlobAssetScriptableObject<T> : ScriptableObject where T : 
         );
         return assetHash;
     }
-        
+    
     public BlobAssetReference<T> GetBlobDataReference(IBaker baker)
     {
         Hash128 hashCode = new Hash128((uint)GetHashCode());
