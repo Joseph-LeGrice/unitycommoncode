ae21d856a187b6e466fa4d07d71dccb36f2f4d8b
Joseph Le Grice <joe.legrice@gmail.com>
2025-07-14 13:16:12 +0100

    Added MathHelpers.RotateTowards

--- Runtime/Entities/MathHelpers.cs
+++ Runtime/Entities/MathHelpers.cs
@@ -13,12 +13,26 @@ public static class MathHelpers
         return math.acos(math.clamp(math.dot(from, to), -1f, 1f));
     }
     
-    public static quaternion GetFromToRotation(float3 from , float3 to)
+    public static quaternion GetFromToRotation(float3 from, float3 to)
     {
         from = math.normalize(from);
         to = math.normalize(to);
         
         float angle = math.acos(math.clamp(math.dot(from, to), -1f, 1f));
+        float3 axis = math.cross(from, to);
+        return quaternion.AxisAngle(axis, angle);
+    }
+    
+    public static quaternion RotateTowards(float3 from, float3 to, float maxDegreesDelta)
+    {
+        from = math.normalize(from);
+        to = math.normalize(to);
+        
+        float angle = math.acos(math.clamp(math.dot(from, to), -1f, 1f));
+        angle = math.degrees(angle);
+        angle = math.sign(angle) * math.min(math.abs(angle), maxDegreesDelta);
+        angle = math.radians(angle);
+        
         float3 axis = math.cross(from, to);
         return quaternion.AxisAngle(axis, angle);
     }
